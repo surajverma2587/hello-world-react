62eb67dccc9438b0e4e061a66e8174478b359e87
surajverma2587 <surajverma2587@gmail.com>
2023-10-17 20:20:38 +0100

    complete introduction omponent

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,3 +1,22 @@
 export const Introduction = () => {
-  return <div>Introduction</div>;
+  return (
+    <div className="introduction-container">
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1287"
+          alt="profile picture of a women smiling in a red top"
+          className="profile-img"
+        />
+      </div>
+      <div>
+        <h1>Alice Smith</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+          consequatur ducimus ut doloribus accusantium obcaecati voluptatem
+          voluptates eligendi laudantium, harum quis repudiandae enim illum
+          dolorem perspiciatis, quasi molestiae rem nihil!
+        </p>
+      </div>
+    </div>
+  );
 };
