5550628eb6d3375b5759847756c7535f6789167d
surajverma2587 <surajverma2587@gmail.com>
2023-10-17 20:01:58 +0100

    add files and folders

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import { Introduction } from "./components/Introduction";
+import { List } from "./components/List";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Introduction />
+      <List />
+      <List />
+    </div>
+  );
 };
